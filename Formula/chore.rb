04a4a4465c3efc2668cf7398fffde8c31f743e2f
Zilong Liang <zlliang96@outlook.com>
2026-05-01 16:08:23 +0800

    chore: update to 0.1.4

--- Formula/chore.rb
+++ Formula/chore.rb
@@ -1,26 +1,26 @@
 class Chore < Formula
   desc "Task runner for repetitive daily chores"
   homepage "https://github.com/zlliang/chore"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/zlliang/chore/releases/download/v#{version}/chore_#{version}_darwin_arm64.tar.gz"
-      sha256 "608d58b7e7304de6d5fbc54fa4e6c3899e8d607dd491eace40f581f5c0b7e5ce"
+      sha256 "859c5eaedb32f8f8f055b3672c4b307f713ba74d476ee18e5164d4b150041dde"
     elsif Hardware::CPU.intel?
       url "https://github.com/zlliang/chore/releases/download/v#{version}/chore_#{version}_darwin_amd64.tar.gz"
-      sha256 "a043025ed690278e64f2a230e7fcaa2bd2f01c50cacae937715bdc047595bc61"
+      sha256 "72f205c53e709e2172b4fb31a4a4c8362be7fedd0560518154245bf1a9b55cf6"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/zlliang/chore/releases/download/v#{version}/chore_#{version}_linux_arm64.tar.gz"
-      sha256 "4c855280c3b82be66721ce3e8397461abe93b8890066f98be05eedcfdbed76c9"
+      sha256 "a36636ffb6c25db26dc8c1eb9570453e5e942191fc88c6fda0709087e91ff979"
     elsif Hardware::CPU.intel?
       url "https://github.com/zlliang/chore/releases/download/v#{version}/chore_#{version}_linux_amd64.tar.gz"
-      sha256 "79038e892bc8e19aa9d9affd1a53f9861462da20deb3e1f9497ce559329380fa"
+      sha256 "272b7a32f0cc2f3f0cb8cd06f8d74d8ea81c584a2826afdf2641c794f0375d48"
     end
   end
 
